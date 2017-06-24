40759d094143a58970c28dde3c5da2a7d0b8b41f
Raviraj Magadum <raviraj.magadum@gmail.com>
2017-06-24 11:40:47 +0530

    Commited the changes for Larevel auth

---
+++ database/migrations/2017_06_24_055545_alterUserTable.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function($table) {
+            $table->engine = 'InnoDB';
+            if (Schema::hasColumn('users', 'name')) {
+               $table->dropColumn('name');
+            }
+            $table->string('first_name')->after('id')->default(null);
+            $table->string('last_name')->after('first_name')->default(null);
+            $table->string('role')->after('last_name')->default(2);
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function(Blueprint $table) {
+            $table->dropColumn('first_name');
+            $table->dropColumn('last_name');
+            $table->dropColumn('role');
+        });
+    }
+}
